455e9fec9c23deffb50396297f01bd4fdbe0bc39
User <User@DESKTOP-AMQNG48>
2021-05-13 20:57:20 -0500

    PRUEBA A

--- src/minombre/pkg66132/MiNombre66132.java
+++ src/minombre/pkg66132/MiNombre66132.java
@@ -10,14 +10,21 @@ package minombre.pkg66132;
  * @author User
  */
 public class MiNombre66132 {
+    
+    static void encabezados() {
+        System.out.println("UNIVERSIDAD AUTÓNOMA DE CAMPECHE");
+        System.out.println("FACULTAD DE INGENIERÍA");
+        System.out.println("INGENIERÍA EN SISTEMAS COMPUTACIONALES");
+        System.out.println("ÁNGEL GABRIEL MANRERO HIDALGO");
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
         String minombre = "Angel Gabriel Manrero Hidalgo";
-        System.out.println("Mi nombre es: + minombre");
+        System.out.println("Mi nombre es: "+ minombre);
     }
     
 }
